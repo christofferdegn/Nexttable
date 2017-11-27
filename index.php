632efbd97a727cb673bec38d585310d5ef024ffa
christofferdegn <31208214+christofferdegn@users.noreply.github.com>
2017-11-27 17:28:10 +0100

    start

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title></title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+
+<section id="nexttablesection">
+<h1>Nexttable</h1>
+
+<button class="table" id="table_1"></button>
+
+<button class="table" id="table_2"></button>
+
+<br>
+
+<button class="table" id="table_3"></button>
+
+<button class="table" id="table_4"></button>
+
+</section>
+
+</body>
+</html>
